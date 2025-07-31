e96ca6445c90363f22322d46afb3eb6fb437145c
KevR98 <ramil.kevin98@gmail.com>
2025-07-31 17:34:37 +0200

    done the ex.5

--- src/tests/SingleBook.test.jsx
+++ src/tests/SingleBook.test.jsx
@@ -1,13 +1,20 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import SingleBook from '../components/SingleBook';
+import fantasy from '../data/fantasy.json';
 
 describe('check the border changing', () => {
   it('makes the border red', () => {
-    render(<SingleBook />);
+    render(
+      <SingleBook
+        book={fantasy[0]}
+        changeSelectedBook={() => {}}
+        selectedBook={fantasy[0].asin}
+      />
+    );
 
-    const card = screen.queryByTestId('border-red');
+    const card = screen.getByTestId('border-red');
     fireEvent.click(card);
-    expect(card).toHaveStyle('red');
+    expect(card).toHaveStyle('border: 3px solid red');
   });
 });
